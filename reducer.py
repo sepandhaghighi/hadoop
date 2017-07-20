f3ae7ed644a23910924d7f80d4ec63eb82313844
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-07-21 00:09:18 +0430

    fix : sort function solved

--- reducer.py
+++ reducer.py
@@ -13,8 +13,12 @@ def sort(input_list):
     temp_list=input_list
     length=len(input_list)
     output=[]
+    length_list=list(map(len,input_list))
     for i in range(length):
-        output.append(temp_list.pop(temp_list.index(max(temp_list))))
+        max_item=max(length_list)
+        max_index=length_list.index(max_item)
+        length_list.remove(max_item)
+        output.append(temp_list.pop(max_index))
     return output
 
 def main(separator='\t'):
