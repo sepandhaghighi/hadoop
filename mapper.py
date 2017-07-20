2eb8b008544da9d4a4afde35e5ffc33ae3735b54
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-07-20 23:53:18 +0430

    fix : reducer updated

--- mapper.py
+++ mapper.py
@@ -1,18 +1,13 @@
 import sys
 import re
-
-
 def read_input(file):
     for line in file:
         line = re.split('[^a-z]+', line.lower())
         yield line
-
-
 def main(separator='\t'):
     for words in read_input(sys.stdin):
         for word in words:
             if len(word) > 0:
                 print '%s%s%s' % (''.join(sorted(word)), separator, word)
-
 if __name__ == "__main__":
     main()
--- reducer.py
+++ reducer.py
@@ -1,24 +1,30 @@
+#!/usr/bin/python
+
 from itertools import groupby
 from operator import itemgetter
 import sys
 
-
+output=[]
 def read_mapper_output(file, separator='\t'):
     for line in file:
         yield line.rstrip().split(separator, 1)
 
+def sort(input_list):
+    temp_list=input_list
+    length=len(input_list)
+    output=[]
+    for i in range(length):
+        output.append(temp_list.pop(temp_list.index(max(temp_list))))
 
 def main(separator='\t'):
     data = read_mapper_output(sys.stdin, separator=separator)
-    min_length=0
     final_list=[]
     for current_word, group in groupby(data, itemgetter(0)):
         anagram_list = list(set(anagram for current_word, anagram in group))
-        anagram_length=len(anagram_list)
-        if anagram_length > 2 and anagram_length>min_length :
+        if anagram_length > 2:
             final_list.append(anagram_list)
-            min_length=anagram_length
-    for item in final_list:
+    sort(final_list)
+    for item in output:
         print "%s\t%s" % (len(item), item)
 
 
